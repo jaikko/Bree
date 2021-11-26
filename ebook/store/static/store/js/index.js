21049630b0ee010bb7d11f43b2e40c4a01ffee4d
jaikko <65661383+jaikko@users.noreply.github.com>
2021-11-26 15:34:08 +0100

    add research bar

--- ebook/store/static/store/js/index.js
+++ ebook/store/static/store/js/index.js
@@ -1,20 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-    /* ... */
-    const hamburger = document.querySelector(".hamburger");
-    const navLinks = document.querySelector(".nav-links");
-    const links = document.querySelectorAll(".nav-links li");
+$(document).ready(function() {
+    $(".searchButton").click(function() {
+        var inputValue = $(".searchTerm").val().trim();
+        const csrftoken = Cookies.get('csrftoken');
+        const request = new Request(
+            "http://127.0.0.1:8000/book/?title=" + inputValue, {
+                headers: {
+                    'X-CSRFToken': csrftoken,
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+        var status;
+        fetch(request, {
+            method: 'get',
+        }).then(response => {
+            status = response.status;
+            return response.json();
+        }).then(data => {
+            $(".cards").remove();
+            console.log(data.length);
+            const obj = JSON.stringify(data);
+            console.log(obj);
+            $.each(JSON.parse(obj), function(i, obj) {
+                $(".grid").append('<div class="cards"><div class="card"><div class="card_img"><img src="test"><span></span>');
+                // $(".cards").append('<div class="card"><div>');
+                // $("card").append('<div class="card_img"><div>')
+                // $(".card").html('<div class="card_img"><img src="test"><span></span><div>');
+                // $("#img_book").attr("src", '{% store/img/" + obj.image + ".jpg" + "%}');
+                $('.card_img').find('img').attr('src', '/static/store/img/' + obj.image + '.jpg');
+                $('.card_img').find('span').text(obj.author + '-' + obj.title);
 
-    hamburger.addEventListener('click', () => {
-        //Animate Links
-        navLinks.classList.toggle("open");
-        links.forEach(link => {
-            link.classList.toggle("fade");
+
+                console.log(obj.title);
+            });
+            // for (var i = 0; i < 6; i++) {
+            //     $(".grid").append("<b>Wow!</b> Such excitement..." + i)
+            // }
+
+            // const obj = JSON.stringify(data);
+            // const json = JSON.parse(obj);
+            // var access = json.access;
+            // var refresh = json.refresh;
         });
-
-        //Hamburger Animation
-        hamburger.classList.toggle("toggle");
     });
-
-
-
-})
+});
